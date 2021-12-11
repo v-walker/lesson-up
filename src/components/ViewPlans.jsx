26a1081dba23b85b98485bfa5bbe639bd2ac34c8
Victoria Walker <vmsmith2014@gmail.com>
2021-12-11 12:21:28 -0500

    search date variables setup

--- src/components/ViewPlans.jsx
+++ src/components/ViewPlans.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { deletePlan } from '../actions/planActions';
 
@@ -12,20 +12,7 @@ import Moment from 'react-moment';
 import { MdDeleteForever, MdLocalPrintshop } from 'react-icons/md'
 
 
-function ViewPlans() {
-    const dispatch = useDispatch();
-    const savedPlans = useSelector(state => state.planCRD.fullPlans);
-
-    console.log(savedPlans);
-
-    const handleDelete = (id) => {
-        // https://react-bootstrap.github.io/components/modal/ for later refining of delete functionality with modals
-        dispatch(deletePlan(id));
-
-        alert("Say goodbye to your plan!");
-    }
-
-function tConvert (time) {
+function tConvert(time) {
     // Check correct time format and split into components
     time = time.toString ().match (/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [time];
     if (time.length > 1) { // If time format correct
@@ -36,6 +23,31 @@ function tConvert (time) {
     return time.join (''); // return adjusted time or original string
     }
 
+
+
+function ViewPlans() {
+    const dispatch = useDispatch();
+    const savedPlans = useSelector(state => state.planCRD.fullPlans);
+
+    // local state variables
+    const [searchDate, setSearchDate] = useState("")
+
+    console.log(savedPlans);
+
+    useEffect(() => {
+        
+
+    }, [searchDate])
+
+    const handleDelete = (id) => {
+        // https://react-bootstrap.github.io/components/modal/ for later refining of delete functionality with modals
+        dispatch(deletePlan(id));
+
+        alert("Say goodbye to your plan!");
+    }
+
+    
+
     return (
         <div className="container">
             <div className="row d-flex justify-content-center">
@@ -43,12 +55,18 @@ function tConvert (time) {
                     <h3>Search Plans By Week</h3>
                         <Form>
                             <Form.Group>
-                                <Form.Label>Select starting date for week</Form.Label>
-                                <Form.Control type="date" onChange></Form.Control>
+                                <Form.Label>Select starting date for the week you would like to search for</Form.Label>
+                                <Form.Control value={searchDate} type="date" onChange={(e) => setSearchDate(e.target.value)}></Form.Control>
                             </Form.Group>
                         </Form>
 
+                        {searchDate && 
+                            <>
+                                <h3>Plans Matching Your Search</h3>
+                            </>
+                        }
 
+                    {/* List of all saved plans... maybe limit the length of these to 10-15... */}
                     <h3>Your Saved Plans</h3>
                     <br />
                     
