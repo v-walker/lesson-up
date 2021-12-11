39a9ce269fd96d586e4087051c6a3a8818c98d04
Victoria Walker <vmsmith2014@gmail.com>
2021-12-11 16:18:54 -0500

    moved tConvert function to utils

--- src/components/PlanCard.jsx
+++ src/components/PlanCard.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import Moment from 'react-moment';
 
 // local components
+import { tConvert } from '../utils';
 import { addToPrintList, removeFromPrintList } from '../actions/planActions';
 
 // Bootstrap components
@@ -13,17 +14,6 @@ import Button from 'react-bootstrap/Button';
 // icons
 import { MdDeleteForever, MdLocalPrintshop } from 'react-icons/md'
 import { CgPlayListRemove } from 'react-icons/cg'
-
-function tConvert(time) {
-    // Check correct time format and split into components
-    time = time.toString ().match (/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [time];
-    if (time.length > 1) { // If time format correct
-        time = time.slice (1);  // Remove full string match value
-        time[5] = +time[0] < 12 ? 'AM' : 'PM'; // Set AM/PM
-        time[0] = +time[0] % 12 || 12; // Adjust hours
-    }
-    return time.join (''); // return adjusted time or original string
-    }
 
 function PlanCard({planObj, handleDelete}) {
     const dispatch = useDispatch();
