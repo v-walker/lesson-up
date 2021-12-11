bf3a65a107e10bf0dcf6357805a9f0d11a4940fa
Victoria Walker <vmsmith2014@gmail.com>
2021-12-11 17:02:53 -0500

    added link to print view

--- src/components/PrintSidePlanel.jsx
+++ src/components/PrintSidePlanel.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 // local components
 import { tConvert } from '../utils';
@@ -7,6 +8,7 @@ import { removeFromPrintList } from '../actions/planActions';
 
 // library components
 import Moment from 'react-moment';
+import Button from 'react-bootstrap/Button';
 
 // icons
 import { CgPlayListRemove } from 'react-icons/cg'
@@ -21,13 +23,18 @@ function PrintSidePlanel() {
         <div className="ms-5">
             <h4>Print List</h4>
 
-            <ul className="mt-5">
+            <ul className="mt-5 mb-5">
                 {printList.map(planObj => {
                     return (
                         <li> <CgPlayListRemove style={{color: "red"}} onClick={() => dispatch(removeFromPrintList(planObj.id))} /> {tConvert (planObj.data.time)} {planObj.data.gradeLevel} {planObj.data.subject[0]} plan for week of&nbsp;<Moment format="MM/DD/YYYY">{planObj.data.weekOf}</Moment></li>
                     )
                 })}
             </ul>
+
+            <div className="text-end">
+                <Button className="btn btn-warning"><Link to="/print_plans">Go to Print View</Link></Button>
+            </div>
+
         </div>
     )
 }
