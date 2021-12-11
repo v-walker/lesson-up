5c37e272e1964c7375461e3a93f9f7b26c062315
Victoria Walker <vmsmith2014@gmail.com>
2021-12-10 19:00:36 -0500

    plan print layout drafted

--- src/components/IndividualPlan.jsx
+++ src/components/IndividualPlan.jsx
@@ -3,20 +3,33 @@ import IndividualPlanDay from './IndividualPlanDay';
 
 function IndividualPlan() {
     return (
-        <>
+        <div>
+            
+            <div style={{display: "flex", justifyContent: "center", fontSize: "0.8rem"}}>
             <div className="container m-5" style={{border: "0.5px solid black"}}>
-                <div className="row d-flex justify-content-center" style={{backgroundColor: "yellow", border: "1px solid black"}}>
-                    Time, Grade and Subject Area (darker background color)
+                <div className="row">
+                    <div className="text-center pt-2 pb-2" width="100%" style={{backgroundColor: "yellow"}}>
+                        <b>Time, Grade and Subject Area</b>
+                    </div>
+                    <hr className="m-0" />
                 </div>
-
-                <div className="row" style={{backgroundColor: "white", border: "0.5px solid black"}}>
+                
+                
+                <div className="row" style={{backgroundColor: "white"}}>
                     <div className="col-12">
-                        Standards:
+                        <b>Week of:</b>
                     </div>
-                    <hr />
-                    <div>
-                        Content Vocabulary:
+                    <hr className="m-0"/>
+                    <div className="col-12">
+                        <b>Standard(s):</b>
+                        <br />
+                        {/* standards here */}
                     </div>
+                    <hr className="m-0"/>
+                    <div className="col-12">
+                        <b>Content Vocabulary:</b> &nbsp; {/* content vocab here */}
+                    </div>
+                    <hr className="m-0" />
                 </div>
 
                 <div className="row">
@@ -32,7 +45,9 @@ function IndividualPlan() {
                 </div>
             </div>
             
-        </>
+        </div>
+        </div>
+        
     )
 }
 
--- src/components/IndividualPlanDay.jsx
+++ src/components/IndividualPlanDay.jsx
@@ -2,14 +2,40 @@ import React from 'react';
 
 function IndividualPlanDay() {
     return (
-        <div className="m-0 p-0" style={{width: "20%"}}>
+        <div className="m-0 p-0" style={{width: "20%", borderRight: "0.5px solid black"}}>
             <div className="row">
                 <div className="col-12">
-                    <div className="text-center" style={{borderBottom: "0.5px solid black"}}>
-                        Day
+                    <div className="text-center" style={{backgroundColor: "yellow"}}>
+                        <b>Day</b>
+                        <hr className="m-0" />
                     </div>
-                    
-                    
+                    <div className="ms-2">
+                        <b>Learning Target:</b> 
+                    </div>
+                    <hr />
+
+                    <div className="ms-2">
+                        <p>
+                            <b>Hook: </b>
+                            <br />
+                        </p>
+                        <p>
+                            <b>Lesson:</b>
+                            <br />
+                        </p>
+                        <p>
+                            <b>Closing:</b>
+                            <br />
+                        </p>   
+                    </div>  
+                    <hr />                  
+                    <div className="ms-2">
+                        <b>Assessment Method(s)/Graded Assignment(s):</b> 
+                    </div>
+                    <hr />
+                    <div className="ms-2">
+                        <b>Accommodations:</b> 
+                    </div>
                 </div>
             </div>
         </div>
